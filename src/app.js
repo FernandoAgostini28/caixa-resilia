0091b230a48ec9467383522e90fe43dcf3ef3981
FernandoAgostini28 <fernando.agostini.28@gmail.com>
2021-11-27 00:29:27 -0300

    criando arquivo server

--- src/app.js
+++ src/app.js
@@ -1,11 +1,7 @@
 const express = require('express')
 const app = express()
-const port  = 3003
 const caixa  = require('./controller/caixa-controller')
-//const bd =  require('./infra/bd')
-//const sqlite3 = require('sqlite3').verbose();
-//const bd = require('./src/infra/db')
-const bdVerdadeiro = require('./infra/sqlite-db');
+const bdCaixa = require('./infra/sqlite-db');
 // Para acessar middleware para uma rota exata
 // app.use('/user/:id', function (req, res, next) {
 //   console.log('Request Type:', req.method);
@@ -15,36 +11,22 @@ const bdVerdadeiro = require('./infra/sqlite-db');
 
 app.use(express.json())
 app.use((req, res, next) => {
-  console.log(req.body)
-  const body = req.body
-  // if(!body.usuarios){
-  //   res.status(403).send('Quem é vc')
-  // }
-  // if(body.usuarios === "Fer") {
-  //   res.status(403).send('Vc não pode logar')
-  // }
-  // else {
-  next()
- //}
+const body = req.body
+next()
+
   
 })
 
-app.get('/', (req, res)=>{
-       
-    res.json({"requisicao": "Requisição aceita"})
-   
-  })
 
-caixa(app , bdVerdadeiro)
+
+caixa(app, bdCaixa)
 //Lista de rotas
 
 // Middlewares
 
+module.exports = app;
 
 
 
 
-app.listen(port, () =>{
-  console.log(`servidor rodando: http://localhost:${port}`)
-})
 
